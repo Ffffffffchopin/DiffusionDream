ace1e70f3ae6ea39accf7360ec82ca16b56168ea
xsy <1131585528@qq.com>
2024-10-05 00:06:16 +0800

    modified pipeline_instruct_pix2pix

--- inference/pipeline_instruct_pix2pix.py
+++ inference/pipeline_instruct_pix2pix.py
@@ -4,14 +4,19 @@ import torch
 
 generator = torch.Generator(device="cuda").manual_seed(0)
 
-image = PIL.Image.open("屏幕截图 2024-10-04 162903.png")
-image = PIL.ImageOps.exif_transpose(image)
+image = PIL.Image.open("test.png")
+#image = PIL.ImageOps.exif_transpose(image)
 image = image.convert("RGB")
 
-pipeline = StableDiffusionInstructPix2PixPipeline.from_pretrained("fffffchopin/Instruct_pix2pix").to("cuda")
+#pipeline = StableDiffusionInstructPix2PixPipeline.from_pretrained("timbrooks/instruct-pix2pix").to("cuda")
+pipeline = StableDiffusionInstructPix2PixPipeline.from_pretrained("/root/autodl-tmp/model/instruct_pix2pix").to("cuda")
 #pipeline.set_progress_bar_config(disable=True)
 
+prompt="Zoom into the image"
 
-ret=pipeline("tx:0.0215,ty:0.0065,tz:-0.0296,dx:0.0020,dy:-0.0023",image,num_inference_steps=20,image_guidance_scale=1.5,guidance_scale=7,generator=generator).images[0]
+action="tx:0.0215,ty:0.0065,tz:-0.0296,dx:0.0020,dy:-0.0023"
+
+
+ret=pipeline(action,image,num_inference_steps=999,image_guidance_scale=1.5,guidance_scale=7,generator=generator).images[0]
 
 ret.save("output.png")
