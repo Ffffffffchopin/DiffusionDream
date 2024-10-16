312d97eb403df2208b4db7b1aceb8adb9515ed76
xsy <1131585528@qq.com>
2024-10-16 17:36:04 +0800

    unset fp16

--- train/train_instruct_pix2pix.py
+++ train/train_instruct_pix2pix.py
@@ -382,7 +382,7 @@ def parse_args():
     parser.add_argument(
         "--mixed_precision",
         type=str,
-        default='fp16',
+        default='no',
         choices=["no", "fp16", "bf16"],
         help=(
             "Whether to use mixed precision. Choose between fp16 and bf16 (bfloat16). Bf16 requires PyTorch >="
