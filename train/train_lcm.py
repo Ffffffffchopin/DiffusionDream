a0c113c4c7bef2b8854c2a23e039eb7f113473bf
fffchopin <1131585528@qq.com>
2024-11-27 23:18:16 +0800

    sync

--- train/train_lcm.py
+++ train/train_lcm.py
@@ -1,2 +1,19 @@
 from train_config import get_config
+from pathlib import Path
+from accelerate.utils import ProjectConfiguration, set_seed
+from accelerate import Accelerator
+
+
+def main():
+    train_config = get_config("lcm_config.json")
+    
+    logging_dir = Path(train_config.output_dir, train_config.logging_dir)
+    accelerator_project_config = ProjectConfiguration(project_dir=logging_dir.output_dir, logging_dir=logging_dir)
+    accelerator = Accelerator(
+        gradient_accumulation_steps=train_config.gradient_accumulation_steps,
+        mixed_precision=train_config.mixed_precision,
+        //log_with=args.report_to,
+        project_config=accelerator_project_config,
+        split_batches=True,  # It's important to set this to True when using webdataset to get the right number of steps for lr scheduling. If set to False, the number of steps will be devide by the number of processes assuming batches are multiplied by the number of processes
+    )
 
