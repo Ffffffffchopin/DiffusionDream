609c8b8ddd60100243ed90c31a50701d738aaa74
fffchopin <1131585528@qq.com>
2024-11-27 16:16:22 +0000

    sync
    Signed-off-by: fffchopin <1131585528@qq.com>

--- train/train_lcm.py
+++ train/train_lcm.py
@@ -2,6 +2,8 @@ from train_config import get_config
 from pathlib import Path
 from accelerate.utils import ProjectConfiguration, set_seed
 from accelerate import Accelerator
+import logging
+from accelerate.logging import get_logger
 
 
 def main():
@@ -10,10 +12,17 @@ def main():
     logging_dir = Path(train_config.output_dir, train_config.logging_dir)
     accelerator_project_config = ProjectConfiguration(project_dir=logging_dir.output_dir, logging_dir=logging_dir)
     accelerator = Accelerator(
-        gradient_accumulation_steps=train_config.gradient_accumulation_steps,
+ gradient_accumulation_steps=train_config.gradient_accumulation_steps,
         mixed_precision=train_config.mixed_precision,
-        //log_with=args.report_to,
+        #log_with=args.report_to,
         project_config=accelerator_project_config,
-        split_batches=True,  # It's important to set this to True when using webdataset to get the right number of steps for lr scheduling. If set to False, the number of steps will be devide by the number of processes assuming batches are multiplied by the number of processes
+        split_batches=True, 
     )
+    logging.basicConfig(
+        format="%(asctime)s - %(levelname)s - %(name)s - %(message)s",
+        datefmt="%m/%d/%Y %H:%M:%S",
+        level=logging.INFO,
+    )
+    logger = get_logger(__name__)
+    logger.info(accelerator.state, main_process_only=False)
 
